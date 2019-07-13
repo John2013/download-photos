3e034f0640631d7308594e00905896975fd21aa4
Evgeny Timofeyev <nawasington@ya.ru>
2019-07-13 16:12:57 +0300

    применён асинхронный процесс

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import asyncio
 import subprocess
 
 
-if __name__ == '__main__':
-    args = ('zip', '-r', '-', 'photos', '-1')
-    process = subprocess.Popen(args, stdout=subprocess.PIPE,
-                               stdin=None,
-                               stderr=subprocess.PIPE)
-    archive, stderr = process.communicate()
+async def archivate():
+    args = b'zip -r - photos -1'
+    process_coro = await asyncio.create_subprocess_shell(
+        args,
+        stdin=None,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    archive_chunk, stderr = await process_coro.communicate()
     with open('archive.zip', 'wb') as zip_file:
-        zip_file.write(archive)
+        zip_file.write(archive_chunk)
+
+
+async def main():
+    await archivate()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
